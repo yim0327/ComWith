29a097b412ae07cf8652133ab9bc148e143988e6
messigod1004 <127476497+messigod1004@users.noreply.github.com>
2024-06-27 21:55:08 +0900

    꼬리 문자열 lv.0
    for문을 이용하여 ex 변수에 해당하지 않는 경우만  str배열엘 추가합니다. 그후 answer에 대한 문자열을 반환하는 형태를 통해 결과가 출력되게 됩니다.

---
+++ Programmers/jungjin/TailString.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+           StringBuilder answer = new StringBuilder();
+        
+        for (String str : str_list) {
+            if (!str.contains(ex)) {
+                answer.append(str);
+            }
+        }
+        
+        return answer.toString();
+    
+    }
+}
