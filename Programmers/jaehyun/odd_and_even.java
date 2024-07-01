c693cdaac8b2eb629f99f05869d5a433ff4251eb
Jaehyun <164978284+jaehyunkor@users.noreply.github.com>
2024-07-02 01:52:54 +0900

    Lv.0 홀짝에 따라 다른 값 반환하기

---
+++ Programmers/jaehyun/odd_and_even.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 0; i <= n; i++) {
+            if ((n % 2 == 0) && (i % 2 == 0)) {
+                answer += i*i;
+            }
+            if ((n % 2 != 0) && (i % 2 != 0)) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
